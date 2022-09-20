6ae1b7dad28f0af9e4bc5607ce279d65fb0ba87b
Guga <103228463+BGuga@users.noreply.github.com>
2022-09-20 15:58:33 +0900

    AppConfig를 통한 DI,IOC에서 Spring을 통한 IOC,DI로 변경

--- src/main/java/SpringBasic/SpringCore/AppConfig.java
+++ src/main/java/SpringBasic/SpringCore/AppConfig.java
@@ -2,27 +2,35 @@ package SpringBasic.SpringCore;
 
 import SpringBasic.SpringCore.discount.DiscountPolicy;
 import SpringBasic.SpringCore.discount.FixDiscountPolicy;
+import SpringBasic.SpringCore.discount.RateDiscountPolicy;
 import SpringBasic.SpringCore.member.MemberRepository;
 import SpringBasic.SpringCore.member.MemberService;
 import SpringBasic.SpringCore.member.MemberServiceImpl;
 import SpringBasic.SpringCore.member.MemoryMemberRepository;
 import SpringBasic.SpringCore.order.OrderService;
 import SpringBasic.SpringCore.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
-
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new FixDiscountPolicy();
+//        return new RateDiscountPolicy();
     }
 }
