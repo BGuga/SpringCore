6260ca1c50db83b685eb0b14d91d3149dc8bf662
Guga <103228463+BGuga@users.noreply.github.com>
2022-09-22 16:46:39 +0900

    lombok을 통한 constructor 생성

--- src/main/java/SpringBasic/SpringCore/order/OrderServiceImpl.java
+++ src/main/java/SpringBasic/SpringCore/order/OrderServiceImpl.java
@@ -6,20 +6,22 @@ import SpringBasic.SpringCore.discount.RateDiscountPolicy;
 import SpringBasic.SpringCore.member.Member;
 import SpringBasic.SpringCore.member.MemberRepository;
 import SpringBasic.SpringCore.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
 
     @Override
